67101f577be9b06fbed34f2302cd940bc4c21e80
jiangtt18 <tingtingjiang18@gmail.com>
2019-12-16 16:10:15 -0800

    create route wrapper

---
+++ src/routes/FitnessRoutes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Fitness from '../App'
+import MyInventory from '../screens/MyInventory/MyInventory';
+import StreamsFormWrapper from '../screens/StreamsForm/StreamsFormWrapper';
+import ErrorBoundary from '../components/Error/ErrorBoundary';
+
+const FitnessRoutes = props => {
+
+	return (
+			<Router>
+				<div>
+					<Route
+						path='/fitness/dashboard'
+						render={({ history, match }) => {
+							return <Fitness history={history} match={match}/>;
+						}}
+					/>
+					{/*<Route*/}
+					{/*	path='/streams/system_reports/:uuid'*/}
+					{/*	render={({ history, match }) => (*/}
+					{/*		<StreamsFormWrapper history={history} match={match} customerData={customerData} userData={userData} />*/}
+					{/*	)}*/}
+					{/*/>*/}
+				</div>
+			</Router>
+	);
+};
+
+export default FitnessRoutes;
