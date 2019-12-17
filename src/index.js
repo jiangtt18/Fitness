badcd2b55cba733f1619a6addebb2d249e8dceec
jiangtt18 <tingtingjiang18@gmail.com>
2019-12-16 16:15:05 -0800

    render routes rather than components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import FitnessRoutes from "./routes/FitnessRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<FitnessRoutes/>,
+	document.getElementById('root')
+);
